73ed3360977bfb62df54b40523a9990089afc4f6
FranciellyPereira <francristinapereira9@gmail.com>
2025-02-13 19:01:30 -0300

    Variáveis e funções no JS

---
+++ aula-0.1-variaiveis-e-funcoes/js-aula-0.1.js
@@ -0,0 +1,22 @@
+//Comentario em linha JS
+/*Cometario em
+ bloco */
+ // Selecionar e apertar CTRL + ; - Atalho para colocar comentario em linha.
+
+ //Aprendendo a declaracao de variaveis no JS
+ // CONST x LET x VAR -> Evitar o uso do VAR
+ // No escopo global CONST não pode ser alterado
+
+ const nome = "Francielly" // Precisa de um valor
+ let Nome // Permite iniciar sem valor
+ // const nome = "Francielly" -> resulta em um erro
+ // let nome -> resulta em erro
+
+ var cor = "Azul"
+ var cor = "Amarelo"
+
+ let cidade = "Registro"
+ cidade = "Jacupiranga" // let permite alterar o valor
+
+ const message = "Hello, word! Iniciando estudos."
+ console.log(message)
